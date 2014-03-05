22e241e6a8ab70fbf1e22da7d1acd8c1c0f15fa1
Tarek Anandan <tarek@technotarek.com>
2014-03-04 19:55:13 -0500

    Removed old revokeAllDestination setting

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ $(document).ready(function()
     $.permit(
         {
             permits: ['free','premium','admin'],
-            reissueDestination: 'reload',
-            revokeDestination: 'http://' + window.location.hostname + '/permit.html'
-        }
+            reissueDestination: 'reload'        }
     );
 });
